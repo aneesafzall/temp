913db5827951d43d047a2f8b1d535c124df5148f
Anees Afzal <P2503259@my365.dmu.ac.uk>
2020-03-19 15:59:17 +0000

    updated file

---
+++ AccessoryTesting/tstAccessories.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AccessoryTesting
+{
+    [TestClass]
+    public class tstAccessories
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
